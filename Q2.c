f6fc184a48961e9220ca19614848381af9aa36f4
ZainDar12 <k250146@nu.edu.pk>
2025-11-24 22:03:32 +0500

    Add files via upload

---
+++ Q2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+int calculateFuel(int fuel,int consumption,int recharge,int SolarBonus,int planet,int totalPlanets,int callNo){
+    if(fuel<0) return 0;
+    if(planet>totalPlanets)return 1;
+    fuel-=consumption;
+    // assume every 5th planet gives gravitational assist
+    fuel+=callNo%4==0?SolarBonus:0;
+    fuel+=callNo%5==0?recharge:0;
+    printf("Planet %d: Fuel Remaining = %d\n", planet, fuel);
+    return calculateFuel(fuel,consumption,recharge,SolarBonus,planet+1,totalPlanets,callNo+1);
+}
